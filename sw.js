cc16e3eb765804cbc879bc510ab09fb84c4fb4e2
Sturza-Lab <420avemaria@gmail.com>
2026-08-27 15:05:57 +0700

    Redesign Ritm with soft feminine cat companion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ritm-shell-v4";
+const CACHE = "ritm-shell-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./tracker-core.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
